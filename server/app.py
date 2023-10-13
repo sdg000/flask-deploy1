91c9dfa9c577648f6da35235edb59aa13bd15b8d
Seth David Gyimah <esec26@gmail.com>
2023-10-13 15:31:35 +0000

    removed postgres uri

--- server/app.py
+++ server/app.py
@@ -25,7 +25,6 @@ from model import db, Bird
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tiam1:8SFov96XNa98pwMNlTHGPYSRmvzzGgtb@dpg-ckkjd16a18fc73adl8i0-a.oregon-postgres.render.com/fd1'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
